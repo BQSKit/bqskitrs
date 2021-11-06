ddc563cb28738a802640ef9403801649a6ab9947
Ethan Smith <ethan@ethanhs.me>
2021-11-05 18:04:24 -0700

    Rewrite kron to code that will be upstreamed

--- squaremat/src/kron.rs
+++ squaremat/src/kron.rs
@@ -1,48 +1,44 @@
-use ndarray::{Array2, ArrayView2, ArrayViewMut2};
+use std::mem::MaybeUninit;
+
+use ndarray::{Array2, ArrayBase, ArrayView2, Data, Ix2, LinalgScalar, OwnedRepr, Zip};
 use num_complex::Complex64;
 
 pub trait Kronecker {
     fn kron(&self, other: &Array2<Complex64>) -> Array2<Complex64>;
 }
 
+/// Kronecker product of 2D matrices.
+///
+/// The kronecker product of a LxN matrix A and a MxR matrix B is a (L*M)x(N*R)
+/// matrix K formed by the block multiplication A_ij * B.
+fn kron<'a, A, S1, S2>(a: &ArrayBase<S1, Ix2>, b: &'a ArrayBase<S2, Ix2>) -> ArrayBase<OwnedRepr<A>, Ix2>
+where
+    S1: Data<Elem = A>,
+    S2: Data<Elem = A>,
+    A: LinalgScalar,
+    A: std::ops::Mul<&'a ArrayBase<S2, Ix2>, Output = ArrayBase<OwnedRepr<A>, Ix2>>,
+{
+    let dimar = a.shape()[0];
+    let dimac = a.shape()[1];
+    let dimbr = b.shape()[0];
+    let dimbc = b.shape()[1];
+    let mut out: Array2<MaybeUninit<A>> = Array2::uninit((dimar * dimbr, dimac * dimbc));
+    Zip::from(out.exact_chunks_mut((dimbr, dimbc)))
+        .and(a)
+        .for_each(|out, a| {
+            (*a * b).assign_to(out);
+        });
+    unsafe { out.assume_init() }
+}
+
 impl Kronecker for Array2<Complex64> {
     fn kron(&self, other: &Array2<Complex64>) -> Array2<Complex64> {
-        let row_a = self.shape()[0];
-        let row_b = other.shape()[0];
-        // Safety: out is initialized below.
-        let mut out = Array2::uninit((row_a * row_b, row_a * row_b));
-        for (mut chunk, elem) in out
-            .exact_chunks_mut((row_b, row_b))
-            .into_iter()
-            .zip(self.iter())
-        {
-            let v = Array2::from_elem((row_b, row_b), *(elem)) * other;
-            // safety: the next line assigns values to `chunk_assign`
-            let chunk_assign: &mut ArrayViewMut2<Complex64> =
-                unsafe { std::mem::transmute(&mut chunk) };
-            chunk_assign.assign(&v);
-        }
-        unsafe { out.assume_init() }
+        kron(self, other)
     }
 }
 
 impl Kronecker for ArrayView2<'_, Complex64> {
     fn kron(&self, other: &Array2<Complex64>) -> Array2<Complex64> {
-        let row_a = self.shape()[0];
-        let row_b = other.shape()[0];
-        // Safety: out is initialized below.
-        let mut out = Array2::uninit((row_a * row_b, row_a * row_b));
-        for (mut chunk, elem) in out
-            .exact_chunks_mut((row_b, row_b))
-            .into_iter()
-            .zip(self.iter())
-        {
-            let v = Array2::from_elem((row_b, row_b), *(elem)) * other;
-            // safety: the next line assigns values to `chunk_assign`
-            let chunk_assign: &mut ArrayViewMut2<Complex64> =
-                unsafe { std::mem::transmute(&mut chunk) };
-            chunk_assign.assign(&v);
-        }
-        unsafe { out.assume_init() }
+        kron(self, other)
     }
 }
